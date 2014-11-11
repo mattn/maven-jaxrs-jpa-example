56a218c8df20894e5deda555e0483d12293309f9
mattn <mattn.jp@gmail.com>
2014-11-12 02:31:35 +0900

    Add DDL

---
+++ schema/ddl.sql
@@ -0,0 +1,6 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE foo(id integer primary key, key string, value string);
+-- INSERT INTO "foo" VALUES(1,'ふー','ばー');
+-- INSERT INTO "foo" VALUES(2,'じじー','ばばー');
+-- COMMIT;
